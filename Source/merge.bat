45f2d3608a5bd327c35d514f4483b15ed8f7d01f
HTotoo <ttotoo@gmail.com>
2024-11-02 21:49:25 +0100

    add merger for web flasher

---
+++ Source/merge.bat
@@ -0,0 +1,1 @@
+python -m esptool --chip esp32s3 merge_bin -o build/merged.bin --flash_mode dio --flash_freq 80m --flash_size 8MB 0x0 build/bootloader/bootloader.bin 0x10000 build/ESP32PP.bin 0x8000 build/partition_table/partition-table.bin 0xd000 build/ota_data_initial.bin
