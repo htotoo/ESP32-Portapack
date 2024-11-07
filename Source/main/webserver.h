0287868ad58ce38911ade803a14ce26f06819721
HTotoo <ttotoo@gmail.com>
2024-11-07 14:42:42 +0100

    fixes

--- Source/main/webserver.h
+++ Source/main/webserver.h
@@ -41,120 +41,104 @@ extern const char ota_start[] asm("_binary_ota_html_start");
 extern const char ota_end[] asm("_binary_ota_html_end");
 
 // root / get handler.
-static esp_err_t get_req_handler(httpd_req_t *req)
-{
+static esp_err_t get_req_handler(httpd_req_t* req) {
     const uint32_t index_len = index_end - index_start;
     int response = httpd_resp_send(req, index_start, index_len);
     return response;
 }
 
 /// setup.html get handler
-static esp_err_t get_req_handler_setup(httpd_req_t *req)
-{
+static esp_err_t get_req_handler_setup(httpd_req_t* req) {
     snprintf(setup_html_out, sizeof(setup_html_out), setup_start, WifiM::wifiHostName, WifiM::wifiAPSSID, WifiM::wifiAPPASS, WifiM::wifiStaSSID, WifiM::wifiStaPASS, LedFeedback::get_brightness(), declinationAngle, gps_baud);
     int response = httpd_resp_send(req, setup_html_out, HTTPD_RESP_USE_STRLEN);
     return response;
 }
 /// setup.html get handler
-static esp_err_t get_req_handler_ota(httpd_req_t *req)
-{
+static esp_err_t get_req_handler_ota(httpd_req_t* req) {
     const uint32_t ota_len = ota_end - ota_start;
     int response = httpd_resp_send(req, ota_start, ota_len);
     return response;
 }
 
 // helper function to parse the POST variables
-static int find_post_value(char *key, char *parameter, char *value)
-{
+static int find_post_value(char* key, char* parameter, char* value) {
     // char * addr1;
-    char *addr1 = strstr(parameter, key);
+    char* addr1 = strstr(parameter, key);
     if (addr1 == NULL)
         return 0;
-    ESP_LOGD(TAG, "addr1=%s", addr1);
+    ESP_LOGD("WEBS", "addr1=%s", addr1);
 
-    char *addr2 = addr1 + strlen(key);
-    ESP_LOGD(TAG, "addr2=[%s]", addr2);
+    char* addr2 = addr1 + strlen(key);
+    ESP_LOGD("WEBS", "addr2=[%s]", addr2);
 
-    char *addr3 = strstr(addr2, "&");
-    ESP_LOGD(TAG, "addr3=%p", addr3);
-    if (addr3 == NULL)
-    {
+    char* addr3 = strstr(addr2, "&");
+    ESP_LOGD("WEBS", "addr3=%p", addr3);
+    if (addr3 == NULL) {
         strcpy(value, addr2);
-    }
-    else
-    {
+    } else {
         int length = addr3 - addr2;
-        ESP_LOGD(TAG, "addr2=%p addr3=%p length=%d", addr2, addr3, length);
+        ESP_LOGD("WEBS", "addr2=%p addr3=%p length=%d", addr2, addr3, length);
         strncpy(value, addr2, length);
         value[length] = 0;
     }
-    ESP_LOGI(TAG, "key=[%s] value=[%s]", key, value);
+    ESP_LOGI("WEBS", "key=[%s] value=[%s]", key, value);
     return strlen(value);
 }
 
 // setup.html post handler. saves the config
-static esp_err_t post_req_handler_setup(httpd_req_t *req)
-{
-    char *buf = (char *)malloc(req->content_len + 1);
+static esp_err_t post_req_handler_setup(httpd_req_t* req) {
+    char* buf = (char*)malloc(req->content_len + 1);
     size_t off = 0;
-    while (off < req->content_len)
-    {
+    while (off < req->content_len) {
         /* Read data received in the request */
         int ret = httpd_req_recv(req, buf + off, req->content_len - off);
-        if (ret <= 0)
-        {
-            if (ret == HTTPD_SOCK_ERR_TIMEOUT)
-            {
+        if (ret <= 0) {
+            if (ret == HTTPD_SOCK_ERR_TIMEOUT) {
                 httpd_resp_send_408(req);
             }
             free(buf);
             return ESP_FAIL;
         }
         off += ret;
-        ESP_LOGI(TAG, "root_post_handler recv length %d", ret);
+        ESP_LOGI("WEBS", "root_post_handler recv length %d", ret);
     }
     buf[off] = '\0';
 
     // parse rets
     char tmp[65] = {0};
-    uint8_t changeMask = 1; // wifi
+    uint8_t changeMask = 1;  // wifi
 
-    if (find_post_value((char *)"wifiHostName=", buf, tmp) > 0)
+    if (find_post_value((char*)"wifiHostName=", buf, tmp) > 0)
         strcpy(WifiM::wifiHostName, tmp);
-    if (find_post_value((char *)"wifiAPSSID=", buf, tmp) > 0)
+    if (find_post_value((char*)"wifiAPSSID=", buf, tmp) > 0)
         strcpy(WifiM::wifiAPSSID, tmp);
-    if (find_post_value((char *)"wifiAPPASS=", buf, tmp) > 0)
+    if (find_post_value((char*)"wifiAPPASS=", buf, tmp) > 0)
         strcpy(WifiM::wifiAPPASS, tmp);
-    if (find_post_value((char *)"wifiStaSSID=", buf, tmp) > 0)
+    if (find_post_value((char*)"wifiStaSSID=", buf, tmp) > 0)
         strcpy(WifiM::wifiStaSSID, tmp);
-    if (find_post_value((char *)"wifiStaPASS=", buf, tmp) > 0)
+    if (find_post_value((char*)"wifiStaPASS=", buf, tmp) > 0)
         strcpy(WifiM::wifiStaPASS, tmp);
 
-    if (find_post_value((char *)"rgb_brightness=", buf, tmp) > 0)
-    {
+    if (find_post_value((char*)"rgb_brightness=", buf, tmp) > 0) {
         uint8_t rgb_brightness = (uint8_t)atoi(tmp);
         if (rgb_brightness > 100)
             rgb_brightness = 100;
         LedFeedback::set_brightness(rgb_brightness);
         changeMask |= 2;
     }
-    if (find_post_value((char *)"gps_baud=", buf, tmp) > 0)
-    {
+    if (find_post_value((char*)"gps_baud=", buf, tmp) > 0) {
         uint32_t gps_tmp = (uint32_t)atoi(tmp);
-        if (gps_tmp == 1200 || gps_tmp == 2400 || gps_tmp == 4800 || gps_tmp == 9600 || gps_tmp == 14400 || gps_tmp == 19200 || gps_tmp == 38400 || gps_tmp == 57600 || gps_tmp == 115200)
-        {
+        if (gps_tmp == 1200 || gps_tmp == 2400 || gps_tmp == 4800 || gps_tmp == 9600 || gps_tmp == 14400 || gps_tmp == 19200 || gps_tmp == 38400 || gps_tmp == 57600 || gps_tmp == 115200) {
             gps_baud = gps_tmp;
             changeMask |= 2;
         }
     }
-    if (find_post_value((char *)"declinationAngle=", buf, tmp) > 0)
-    {
-        for (int i = 0; tmp[i] != '\0'; i++) // replace international stuff
+    if (find_post_value((char*)"declinationAngle=", buf, tmp) > 0) {
+        for (int i = 0; tmp[i] != '\0'; i++)  // replace international stuff
         {
             if (i > 64)
                 break;
-            if (tmp[i] == ',')
-            {
+            if (tmp[i] == ',') {
                 tmp[i] = '.';
             }
         }
@@ -180,11 +164,10 @@ static esp_err_t post_req_handler_setup(httpd_req_t *req)
 }
 
 // send data to all ws clients.
-bool ws_sendall(uint8_t *data, size_t len)
-{
+bool ws_sendall(uint8_t* data, size_t len) {
     httpd_ws_frame_t ws_pkt;
     memset(&ws_pkt, 0, sizeof(httpd_ws_frame_t));
-    ws_pkt.payload = (uint8_t *)data;
+    ws_pkt.payload = (uint8_t*)data;
     ws_pkt.len = len;
     ws_pkt.type = HTTPD_WS_TYPE_BINARY;
 
@@ -194,51 +177,42 @@ bool ws_sendall(uint8_t *data, size_t len)
 
     esp_err_t ret = httpd_get_client_list(server, &fds, client_fds);
 
-    if (ret != ESP_OK)
-    {
+    if (ret != ESP_OK) {
         return false;
     }
 
-    for (int i = 0; i < fds; i++)
-    {
+    for (int i = 0; i < fds; i++) {
         int client_info = httpd_ws_get_fd_info(server, client_fds[i]);
-        if (client_info == HTTPD_WS_CLIENT_WEBSOCKET)
-        {
+        if (client_info == HTTPD_WS_CLIENT_WEBSOCKET) {
             httpd_ws_send_frame_async(server, client_fds[i], &ws_pkt);
         }
     }
     return true;
 }
 
-esp_err_t update_post_handler(httpd_req_t *req)
-{
+esp_err_t update_post_handler(httpd_req_t* req) {
     char buf[1000];
     esp_ota_handle_t ota_handle;
     int remaining = req->content_len;
 
-    const esp_partition_t *ota_partition = esp_ota_get_next_update_partition(NULL);
+    const esp_partition_t* ota_partition = esp_ota_get_next_update_partition(NULL);
     ESP_ERROR_CHECK(esp_ota_begin(ota_partition, OTA_SIZE_UNKNOWN, &ota_handle));
 
-    while (remaining > 0)
-    {
+    while (remaining > 0) {
         int recv_len = httpd_req_recv(req, buf, MIN(remaining, sizeof(buf)));
 
         // Timeout Error: Just retry
-        if (recv_len == HTTPD_SOCK_ERR_TIMEOUT)
-        {
+        if (recv_len == HTTPD_SOCK_ERR_TIMEOUT) {
             continue;
 
             // Serious Error: Abort OTA
-        }
-        else if (recv_len <= 0)
-        {
+        } else if (recv_len <= 0) {
             httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Protocol Error");
             return ESP_FAIL;
         }
 
         // Successful Upload: Flash firmware chunk
-        if (esp_ota_write(ota_handle, (const void *)buf, recv_len) != ESP_OK)
-        {
+        if (esp_ota_write(ota_handle, (const void*)buf, recv_len) != ESP_OK) {
             httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Flash Error");
             return ESP_FAIL;
         }
@@ -247,8 +221,7 @@ esp_err_t update_post_handler(httpd_req_t *req)
     }
 
     // Validate and switch to new OTA image and reboot
-    if (esp_ota_end(ota_handle) != ESP_OK || esp_ota_set_boot_partition(ota_partition) != ESP_OK)
-    {
+    if (esp_ota_end(ota_handle) != ESP_OK || esp_ota_set_boot_partition(ota_partition) != ESP_OK) {
         httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Validation / Activation Error");
         return ESP_FAIL;
     }
@@ -262,66 +235,55 @@ esp_err_t update_post_handler(httpd_req_t *req)
 }
 
 // send to ws clients, the pp disconnected
-void ws_notify_dc()
-{
-    char *data = (char *)"#$##$$#USB_DC\r\n";
-    ws_sendall((uint8_t *)data, 15);
+void ws_notify_dc() {
+    char* data = (char*)"#$##$$#USB_DC\r\n";
+    ws_sendall((uint8_t*)data, 15);
 }
 // send to ws clients, the pp connected
-void ws_notify_cc()
-{
-    char *data = (char *)"#$##$$#USB_CC\r\n";
-    ws_sendall((uint8_t *)data, 15);
+void ws_notify_cc() {
+    char* data = (char*)"#$##$$#USB_CC\r\n";
+    ws_sendall((uint8_t*)data, 15);
 }
-void ws_notify_dc_i2c()
-{
-    char *data = (char *)"#$##$$#I2C_DC\r\n";
-    ws_sendall((uint8_t *)data, 15);
+void ws_notify_dc_i2c() {
+    char* data = (char*)"#$##$$#I2C_DC\r\n";
+    ws_sendall((uint8_t*)data, 15);
 }
 // send to ws clients, the pp connected
-void ws_notify_cc_i2c()
-{
-    char *data = (char *)"#$##$$#I2C_CC\r\n";
-    ws_sendall((uint8_t *)data, 15);
+void ws_notify_cc_i2c() {
+    char* data = (char*)"#$##$$#I2C_CC\r\n";
+    ws_sendall((uint8_t*)data, 15);
 }
 
 // websocket handler
-static esp_err_t handle_ws_req(httpd_req_t *req)
-{
-    if (req->method == HTTP_GET)
-    {
+static esp_err_t handle_ws_req(httpd_req_t* req) {
+    if (req->method == HTTP_GET) {
         return ESP_OK;
     }
 
     httpd_ws_frame_t ws_pkt;
-    uint8_t *buf = NULL;
+    uint8_t* buf = NULL;
     memset(&ws_pkt, 0, sizeof(httpd_ws_frame_t));
     ws_pkt.type = HTTPD_WS_TYPE_TEXT;
     esp_err_t ret = httpd_ws_recv_frame(req, &ws_pkt, 0);
-    if (ret != ESP_OK)
-    {
-        ESP_LOGE(TAG, "httpd_ws_recv_frame failed to get frame len with %d", ret);
+    if (ret != ESP_OK) {
+        ESP_LOGE("WEBS", "httpd_ws_recv_frame failed to get frame len with %d", ret);
         return ret;
     }
 
-    if (ws_pkt.len)
-    {
-        buf = (uint8_t *)calloc(1, ws_pkt.len + 1);
-        if (buf == NULL)
-        {
-            ESP_LOGE(TAG, "Failed to calloc memory for buf");
+    if (ws_pkt.len) {
+        buf = (uint8_t*)calloc(1, ws_pkt.len + 1);
+        if (buf == NULL) {
+            ESP_LOGE("WEBS", "Failed to calloc memory for buf");
             return ESP_ERR_NO_MEM;
         }
         ws_pkt.payload = buf;
         ret = httpd_ws_recv_frame(req, &ws_pkt, ws_pkt.len);
-        if (ret != ESP_OK)
-        {
-            ESP_LOGE(TAG, "httpd_ws_recv_frame failed with %d", ret);
+        if (ret != ESP_OK) {
+            ESP_LOGE("WEBS", "httpd_ws_recv_frame failed with %d", ret);
             free(buf);
             return ret;
         }
-        if (strcmp((const char *)ws_pkt.payload, "#$##$$#GETUSBSTATE\r\n") == 0)
-        { // parse here, since we shouldn't sent it to pp
+        if (strcmp((const char*)ws_pkt.payload, "#$##$$#GETUSBSTATE\r\n") == 0) {  // parse here, since we shouldn't sent it to pp
             if ((PPShellComm::getAnyConnected() & 1) == 1)
                 ws_notify_cc();
             else if ((PPShellComm::getAnyConnected() & 2) == 2)
@@ -337,8 +299,7 @@ static esp_err_t handle_ws_req(httpd_req_t *req)
 }
 
 // config web server part
-static httpd_handle_t setup_websocket_server(void)
-{
+static httpd_handle_t setup_websocket_server(void) {
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
     config.max_open_sockets = 10;
 
@@ -385,8 +346,7 @@ static httpd_handle_t setup_websocket_server(void)
                       .handle_ws_control_frames = false,
                       .supported_subprotocol = NULL};
 
-    if (httpd_start(&server, &config) == ESP_OK)
-    {
+    if (httpd_start(&server, &config) == ESP_OK) {
         httpd_register_uri_handler(server, &uri_get);
         httpd_register_uri_handler(server, &uri_getsetup);
         httpd_register_uri_handler(server, &uri_postsetup);
@@ -399,7 +359,6 @@ static httpd_handle_t setup_websocket_server(void)
 }
 
 // start web server, set it up,..
-void init_httpd()
-{
+void init_httpd() {
     server = setup_websocket_server();
 }
--- Source/main/wifim.cpp
+++ Source/main/wifim.cpp
@@ -1,5 +1,7 @@
 #include "wifim.h"
 
+#define TAG "WIFIM"
+
 char WifiM::wifiAPSSID[64] = {0};
 char WifiM::wifiAPPASS[64] = {0};
 char WifiM::wifiStaSSID[64] = {0};
@@ -11,18 +13,15 @@ int WifiM::ap_client_num = 0;
 uint32_t WifiM::last_wifi_conntry = 0;
 bool WifiM::wifi_sta_ok = false;
 
-bool WifiM::getWifiStaStatus()
-{
+bool WifiM::getWifiStaStatus() {
     return wifi_sta_ok;
 }
 
-int WifiM::getWifiApClientNum()
-{
+int WifiM::getWifiApClientNum() {
     return ap_client_num;
 }
 
-void WifiM::initialise_mdns(void)
-{
+void WifiM::initialise_mdns(void) {
     ESP_ERROR_CHECK(mdns_init());
     ESP_ERROR_CHECK(mdns_hostname_set(wifiHostName));
     ESP_ERROR_CHECK(mdns_instance_name_set(wifiHostName));
@@ -30,52 +29,41 @@ void WifiM::initialise_mdns(void)
     ESP_ERROR_CHECK(mdns_service_subtype_add_for_host(NULL, "_http", "_tcp", NULL, "_server"));
 }
 
-void WifiM::event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
-{
-    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
-    {
+void WifiM::event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
         ESP_LOGI(TAG, "WIFI_EVENT_STA_DISCONNECTED");
-        if (ap_client_num <= 0)
-        {
+        if (ap_client_num <= 0) {
             wifi_sta_ok = false;
             // esp_wifi_connect(); // only when no ap clients presents
         }
-    }
-    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_AP_STACONNECTED)
-    {
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_AP_STACONNECTED) {
         // count ap client number
         ap_client_num++;
         ESP_LOGI(TAG, "WIFI_EVENT_AP_STACONNECTED");
-    }
-    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_AP_STADISCONNECTED)
-    {
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_AP_STADISCONNECTED) {
         // count ap client number
         ap_client_num--;
         ESP_LOGI(TAG, "WIFI_EVENT_AP_STADISCONNECTED");
-        if (ap_client_num <= 0)
-        {
-            last_wifi_conntry = 0; // do it now!
+        if (ap_client_num <= 0) {
+            last_wifi_conntry = 0;  // do it now!
         }
     }
 
-    else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
-    {
+    else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
         wifi_sta_ok = true;
         ESP_LOGI(TAG, "IP_EVENT_STA_GOT_IP");
     }
 }
 
-void WifiM::initialise_wifi(void)
-{
+void WifiM::initialise_wifi(void) {
     static bool initialized = false;
-    if (initialized)
-    {
+    if (initialized) {
         return;
     }
     ESP_ERROR_CHECK(esp_netif_init());
-    esp_netif_t *ap_netif = esp_netif_create_default_wifi_ap();
+    esp_netif_t* ap_netif = esp_netif_create_default_wifi_ap();
     assert(ap_netif);
-    esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
+    esp_netif_t* sta_netif = esp_netif_create_default_wifi_sta();
     assert(sta_netif);
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
@@ -92,24 +80,22 @@ void WifiM::initialise_wifi(void)
     initialized = true;
 }
 
-bool WifiM::wifi_apsta()
-{
+bool WifiM::wifi_apsta() {
     wifi_config_t ap_config = {};
-    strcpy((char *)ap_config.ap.ssid, wifiAPSSID);
-    strcpy((char *)ap_config.ap.password, wifiAPPASS);
+    strcpy((char*)ap_config.ap.ssid, wifiAPSSID);
+    strcpy((char*)ap_config.ap.password, wifiAPPASS);
     ap_config.ap.authmode = WIFI_AUTH_WPA_WPA2_PSK;
     ap_config.ap.ssid_len = strlen(wifiAPSSID);
     ap_config.ap.max_connection = CONFIG_AP_MAX_STA_CONN;
-    ap_config.ap.channel = 0; // CONFIG_AP_WIFI_CHANNEL;
+    ap_config.ap.channel = 0;  // CONFIG_AP_WIFI_CHANNEL;
 
-    if (strlen(wifiAPPASS) == 0)
-    {
+    if (strlen(wifiAPPASS) == 0) {
         ap_config.ap.authmode = WIFI_AUTH_OPEN;
     }
 
     wifi_config_t sta_config = {};
-    strcpy((char *)sta_config.sta.ssid, wifiStaSSID);
-    strcpy((char *)sta_config.sta.password, wifiStaPASS);
+    strcpy((char*)sta_config.sta.ssid, wifiStaSSID);
+    strcpy((char*)sta_config.sta.password, wifiStaPASS);
     sta_config.sta.failure_retry_cnt = 1;
 
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_APSTA));
@@ -123,34 +109,28 @@ bool WifiM::wifi_apsta()
     return true;
 }
 
-void WifiM::wifi_loop(uint32_t millis)
-{
+void WifiM::wifi_loop(uint32_t millis) {
     if (ap_client_num > 0 || wifi_sta_ok)
         return;
-    if (millis - last_wifi_conntry > WIFI_CLIENR_RC_TIME)
-    {
+    if (millis - last_wifi_conntry > WIFI_CLIENR_RC_TIME) {
         ESP_LOGI(TAG, "esp_wifi_connect started.");
         esp_wifi_connect();
         last_wifi_conntry = millis;
     }
 }
 
-void WifiM::load_config_wifi()
-{
+void WifiM::load_config_wifi() {
     ESP_LOGI("CONFIG", "load_config_wifi");
     nvs_handle_t nvs_handle;
-    esp_err_t err = nvs_open("wifi", NVS_READWRITE, &nvs_handle); // https://github.com/espressif/esp-idf/blob/v5.1.2/examples/storage/nvs_rw_value/main/nvs_value_example_main.c
-    if (err != ESP_OK)
-    {
+    esp_err_t err = nvs_open("wifi", NVS_READWRITE, &nvs_handle);  // https://github.com/espressif/esp-idf/blob/v5.1.2/examples/storage/nvs_rw_value/main/nvs_value_example_main.c
+    if (err != ESP_OK) {
         printf("Error (%s) opening NVS handle!\n", esp_err_to_name(err));
         strcpy(wifiAPSSID, DEFAULT_WIFI_AP);
         strcpy(wifiAPPASS, DEFAULT_WIFI_PASS);
         strcpy(wifiStaSSID, DEFAULT_WIFI_STA);
         strcpy(wifiStaPASS, DEFAULT_WIFI_PASS);
         strcpy(wifiHostName, DEFAULT_WIFI_HOSTNAME);
-    }
-    else
-    {
+    } else {
         size_t size = sizeof(wifiAPSSID);
         nvs_get_str(nvs_handle, "wifiAPSSID", wifiAPSSID, &size);
         if (strlen(wifiAPSSID) < 1)
@@ -170,8 +150,7 @@ void WifiM::load_config_wifi()
             strcpy(wifiStaPASS, DEFAULT_WIFI_PASS);
         size = sizeof(wifiHostName);
         nvs_get_str(nvs_handle, "wifiHostName", wifiHostName, &size);
-        if (strlen(wifiHostName) < 1)
-        {
+        if (strlen(wifiHostName) < 1) {
             strcpy(wifiHostName, DEFAULT_WIFI_HOSTNAME);
         }
         nvs_commit(nvs_handle);
@@ -180,18 +159,16 @@ void WifiM::load_config_wifi()
     }
 }
 
-void WifiM::save_config_wifi()
-{
+void WifiM::save_config_wifi() {
     ESP_LOGI("CONFIG", "save_config_wifi");
     nvs_handle_t nvs_handle;
     esp_err_t err = nvs_open("wifi", NVS_READWRITE, &nvs_handle);
-    if (err == ESP_OK)
-    {
-        nvs_set_str(nvs_handle, "wifiHostName", (const char *)wifiHostName);
-        nvs_set_str(nvs_handle, "wifiAPSSID", (const char *)wifiAPSSID);
-        nvs_set_str(nvs_handle, "wifiAPPASS", (const char *)wifiAPPASS);
-        nvs_set_str(nvs_handle, "wifiStaSSID", (const char *)wifiStaSSID);
-        nvs_set_str(nvs_handle, "wifiStaPASS", (const char *)wifiStaPASS);
+    if (err == ESP_OK) {
+        nvs_set_str(nvs_handle, "wifiHostName", (const char*)wifiHostName);
+        nvs_set_str(nvs_handle, "wifiAPSSID", (const char*)wifiAPSSID);
+        nvs_set_str(nvs_handle, "wifiAPPASS", (const char*)wifiAPPASS);
+        nvs_set_str(nvs_handle, "wifiStaSSID", (const char*)wifiStaSSID);
+        nvs_set_str(nvs_handle, "wifiStaPASS", (const char*)wifiStaPASS);
         nvs_close(nvs_handle);
         ESP_LOGI("CONFIG", "save_config_wifi ok");
     }
