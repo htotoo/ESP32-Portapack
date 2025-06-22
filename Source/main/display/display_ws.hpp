d11bec0279fba1d56451147d94a33da917a8a767
HTotoo <ttotoo@gmail.com>
2025-06-22 14:23:11 +0200

    ws display

---
+++ Source/main/display/display_ws.hpp
@@ -0,0 +1,22 @@
+#ifndef DISPLAY_WS_HPP
+#define DISPLAY_WS_HPP
+
+#include "displayskeleton.hpp"
+
+bool ws_sendall(uint8_t* data, size_t len, bool asyncmsg);
+
+class Display_Ws : public DisplayGeneric {
+   public:
+    bool init(uint8_t addr) override;
+    void clear() override;
+    void showTitle(const std::string& title) override;
+    void showMainText(const std::string& text) override;
+    void showMainTextMultiline(const std::string& text) override;
+    void draw() override;
+
+   private:
+    std::string title{};
+    std::string mainText{};
+};
+
+#endif  // DISPLAY_WS_HPP
