d40d4efc89cc4a6d4e49642af1f493c08ebf2928
HTotoo <ttotoo@gmail.com>
2025-10-30 15:27:29 +0100

    fix compile warning, fix wifi settings keyboard bug

--- Source/main/main.cpp
+++ Source/main/main.cpp
@@ -506,7 +506,14 @@ void app_main(void) {
     esp_task_wdt_deinit();
 
     temperature_sensor_handle_t temp_sensor = NULL;
-    temperature_sensor_config_t temp_sensor_config = TEMPERATURE_SENSOR_CONFIG_DEFAULT(-10, 80);
+    temperature_sensor_config_t temp_sensor_config = {
+        .range_min = -10,
+        .range_max = 80,
+        .clk_src = TEMPERATURE_SENSOR_CLK_SRC_DEFAULT,
+        .flags = {
+            .allow_pd = 0,
+        },
+    };
     ESP_ERROR_CHECK(temperature_sensor_install(&temp_sensor_config, &temp_sensor));
     ESP_LOGI(TAG, "Enable temperature sensor");
     ESP_ERROR_CHECK(temperature_sensor_enable(temp_sensor));
